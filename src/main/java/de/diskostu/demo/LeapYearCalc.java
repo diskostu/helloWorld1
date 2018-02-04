f1c6a6b6b7ba6298cc41a1ffdee3fc5a09ed5140
diskostu <denny.kluge@gmail.com>
2018-02-04 14:48:59 +0100

    LeaPYear: made the tests succeed.

--- src/main/java/de/diskostu/demo/LeapYearCalc.java
+++ src/main/java/de/diskostu/demo/LeapYearCalc.java
@@ -12,6 +12,14 @@ public class LeapYearCalc {
     }
 
     public boolean isLeapYear(final Integer year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else if (year % 4 == 0) {
+            return true;
+        }
+
         return false;
     }
 }
